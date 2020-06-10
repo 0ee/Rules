2c99f5173598f6b05c3c51aa786c798f6403a2cd
0ee <haokaiyang@cmcm.com>
2020-06-10 13:08:22 +0800

    添加懒饭 App

---
+++ Subscription/Lanfan.js
@@ -0,0 +1,19 @@
+/*
+[Script]
+http-response ^https:\/\/lanfanapp.com\/api\/v1\/user\/(prime.json|page_detail.json)
+[MITM]
+hostname = lanfanapp.com
+*/
+let body = $response.body;
+body = JSON.parse(body);
+console.log(body)
+if (-1 != $request.url.indexOf('prime.json')) {
+    body["content"]["user"]["prime"]["is_prime"] = true;
+    body["content"]["user"]["prime"]["expires_time"] = "2021-01-01 00:00:00";
+    body["content"]["user"]["prime_contract"] = "automatic_renewal";
+    body["content"]["user"]["is_prime"] = true;
+}else if(-1 != $request.url.indexOf('page_detail.json')) {
+    body["content"]["user"]["is_prime"] = true;
+}
+body = JSON.stringify(body);
+$done({body});
