0481fc61b24ac10b52ec7b8aaa3fae9a632a8560
0ee <45503268+0ee@users.noreply.github.com>
2021-03-22 18:52:10 +0800

    Update Lanfan.js

--- Subscription/Lanfan.js
+++ Subscription/Lanfan.js
@@ -9,7 +9,7 @@ body = JSON.parse(body);
 console.log(body)
 if (-1 != $request.url.indexOf('prime.json')) {
     body["content"]["user"]["prime"]["is_prime"] = true;
-    body["content"]["user"]["prime"]["expires_time"] = "2021-01-01 00:00:00";
+    body["content"]["user"]["prime"]["expires_time"] = "2022-01-01 00:00:00";
     body["content"]["user"]["prime_contract"] = "automatic_renewal";
     body["content"]["user"]["is_prime"] = true;
 }else if(-1 != $request.url.indexOf('page_detail.json')) {
