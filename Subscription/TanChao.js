a24266107a08e49b518cb0a51986e0f442cff6d6
0ee <45503268+0ee@users.noreply.github.com>
2023-01-05 11:13:18 +0800

    Update TanChao.js

--- Subscription/TanChao.js
+++ Subscription/TanChao.js
@@ -4,7 +4,7 @@ console.log($request.url)
 console.log(body)
 if (-1 != $request.url.indexOf('/api/maotai/me')) {
     body.data.is_vip = 1;
-    body.data.vip_end_at = "2022-12-30 11:45:00";
+    body.data.vip_end_at = "2024-12-30 11:45:00";
 }
 body = JSON.stringify(body);
 $done({body});
