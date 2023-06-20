b8deea732513dc19a916b8bbab72fe6e1d61e36b
0ee <haokaiyang@cmcm.com>
2023-06-20 16:50:46 +0800

    upgrade

--- Script/BiliBili.Enhanced.response.beta.js
+++ Script/BiliBili.Enhanced.response.beta.js
@@ -151,6 +151,9 @@ const DataBase = {
 									break;
 								case "x/v2/account/mine": { // 账户信息-我的
 									let data = body.data;
+									data.vip_section.title += 'v1';
+									data.vip_section_v2.title += 'v2title';
+									data.vip_section_v2.desc += 'v2desc';
 									data.sections_v2 = Configs.Mine.sections_v2.map(e => {
 										$.log(`e.title = ${e.title}`)
 										//$.log(`e.items = ${JSON.stringify(e.items)}`);
