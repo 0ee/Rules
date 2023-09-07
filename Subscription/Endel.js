8f6e73dc541bca6c4d3fac9555d7a547745e772b
0ee <haokaiyang@cmcm.com>
2023-09-07 14:29:33 +0800

    endel

--- Subscription/Endel.js
+++ Subscription/Endel.js
@@ -1,7 +1,10 @@
 let body = JSON.parse($response.body)
 if (body.subscription) {
     body.subscription.type = "ACTIVE"
+    body.subscription.period = "YEAR"
     body.subscription.store = "APP_STORE"
+    body.subscription.time_left = 999999
+    body.subscription.valid_until = 1924928348
     $done({body:JSON.stringify(body)})
 } else {
     $done({})
