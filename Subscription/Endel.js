ff2132b4ead5829da043570b574cbdbe3c72eea5
0ee <40558rikmdds@gmail.com>
2023-09-25 13:47:07 +0800

    remove Endel

--- Subscription/Endel.js
+++ Subscription/Endel.js
@@ -1,3 +1,5 @@
+// Endel = type=http-response,pattern=^https?:\/\/api-production.endel.io\/v\d\/call,requires-body=1,script-path=https://raw.githubusercontent.com/0ee/Rules/Surge/Subscription/Endel.js
+// api-production.endel.io
 let body = JSON.parse($response.body)
 if (body.subscription) {
     body.subscription.type = "ACTIVE"
