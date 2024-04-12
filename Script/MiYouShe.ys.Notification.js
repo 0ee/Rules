b4aa0e84ee25f802dcf5c917fd68a6a83a028b35
0ee <40558rikmdds@gmail.com>
2024-04-12 19:29:46 +0800

    米游社打开链接

--- Script/MiYouShe.ys.Notification.js
+++ Script/MiYouShe.ys.Notification.js
@@ -2,22 +2,22 @@ let body = $response.body;
 body = JSON.parse(body);
 if (-1 != $request.url.indexOf('/game_record/app/genshin/aapi/widget/v2')) {
     if(!body.data.has_signed){
-        $notification.post('今天还没签到~~~','快去签到啊~~~','',{'action':'open-url','url':body.data.sign_url,'auto-dismiss':0})
+        $notification.post('今天还没签到~~~','快去签到啊~~~','',{'action':'open-url','url':"mihoyobbs://webview?link="+encodeURI(body.data.sign_url),'auto-dismiss':0})
     }
     if(body.data.current_home_coin >= 1800){
-        $notification.post('壶快满了，快去收菜啊~~~','快去收菜啊~~~','',{'action':'open-url','url':'yuanshen://','auto-dismiss':3})
+        $notification.post('壶快满了，快去收菜啊~~~','快去收菜啊~~~','',{'action':'open-url','url':'yuanshen://','auto-dismiss':300})
     }
     if(body.data.current_resin >= 120){
-        $notification.post('体力快满了~~~','快去合成一下啊~~~','',{'action':'open-url','url':'yuanshen://','auto-dismiss':3})
+        $notification.post('体力快满了~~~','快去合成一下啊~~~','',{'action':'open-url','url':'yuanshen://','auto-dismiss':300})
     }
     if(body.data.expeditions.some(item => item.status === 'Finished')){
-        $notification.post('有探索派遣完成了~~~','快去领一下奖励啊~~~','',{'action':'open-url','url':'yuanshen://','auto-dismiss':3})
+        $notification.post('有探索派遣完成了~~~','快去领一下奖励啊~~~','',{'action':'open-url','url':'yuanshen://','auto-dismiss':300})
     }
     if(!body.data.is_extra_task_reward_received){
-        $notification.post('每日委托奖励没领~~~','快去领一下奖励啊~~~','',{'action':'open-url','url':'yuanshen://','auto-dismiss':3})
+        $notification.post('每日委托奖励没领~~~','快去领一下奖励啊~~~','',{'action':'open-url','url':'yuanshen://','auto-dismiss':300})
     }
     if(body.data.finished_task_num < body.data.total_task_num){
-        $notification.post('每日委托都没做啊','每日委托都没做啊~~~','',{'action':'open-url','url':'yuanshen://','auto-dismiss':3})
+        $notification.post('每日委托都没做啊','每日委托都没做啊~~~','',{'action':'open-url','url':'yuanshen://','auto-dismiss':300})
     }
 }
 $done({});
