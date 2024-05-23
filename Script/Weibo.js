933113f8b6e40cc8f94b550489008a15be3639fd
0ee <40558rikmdds@gmail.com>
2024-05-23 15:45:24 +0800

    33

--- Script/Weibo.js
+++ Script/Weibo.js
@@ -513,7 +513,7 @@ if (url.includes("/interface/sdk/sdkad.php")) {
               (i) =>
                 i.itemId === "100505_-_album" || // 我的相册
                 i.itemId === "100505_-_like" || // 赞/收藏
-                i.itemId === "100505_-_watchhistory" || // 浏览记录
+                i.itemId === "100505_-_watchhistory" // 浏览记录
                 // i.itemId === "100505_-_draft" // 草稿箱
                 // i.itemId === "100505_-_pay"  // 我的钱包
               // i.itemId === "100505_-_ordercenter" || // 我的订单
