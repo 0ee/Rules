9fd1b7ac9c1bb84e009ae13fb00dd748b150239c
0ee <45503268+0ee@users.noreply.github.com>
2024-06-26 18:10:20 +0800

    EmbyPremiere

--- Subscription/Emby.js
+++ Subscription/Emby.js
@@ -8,6 +8,30 @@ if ($request.url.indexOf('mb3admin.com/admin/service/registration/validateDevice
     } else {
         $done({})
     }
+} else if ($request.url.indexOf('mb3admin.com/admin/service/registration/validate') != -1) {
+    $done({
+        status: 200,
+        headers: $response.headers,
+        body: '{"featId": "","registered": true,"expDate": "2099-01-01","key": ""}'
+    })
+} else if ($request.url.indexOf('mb3admin.com/admin/service/registration/getStatus') != -1) {
+    $done({
+        status: 200,
+        headers: $response.headers,
+        body: '{"planType": "Lifetime", "deviceStatus": 0, "subscriptions": []}'
+    })
+} else if ($request.url.indexOf('mb3admin.com/admin/service/appstore/register') != -1) {
+    $done({
+        status: 200,
+        headers: $response.headers,
+        body: '{"featId": "","registered": true,"expDate": "2099-01-01","key": ""}'
+    })
+} else if ($request.url.indexOf('mb3admin.com/emby/Plugins/SecurityInfo') != -1) {
+    $done({
+        status: 200,
+        headers: $response.headers,
+        body: '{SupporterKey: "", IsMBSupporter: true}'
+    })
 } else {
     $done({})
 }
