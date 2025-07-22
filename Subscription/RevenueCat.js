6cd5165f9d68881275e6ce68dd602bb922871efa
0ee <40558rikmdds@gmail.com>
2025-07-22 15:11:46 +0800

    feat() goo

--- Subscription/RevenueCat.js
+++ Subscription/RevenueCat.js
@@ -21,15 +21,15 @@ const UA = $request.headers['user-agent'] || $request.headers['User-Agent'] || '
 const appName = UA.split('/')[0] || 'unknown';
 const nonce = $request.headers['x-nonce'] || $request.headers['X-Nonce'];
 const signature = $request.headers['x-signature'] || $request.headers['X-Signature'];
-if (nonce && nonce.trim() !== '') {
-    console.log(`x-nonce detected: ${nonce}`);
-    $notification.post(`${appName} 版本号: ${rcVersion}`, `📴`)
-    $done({});
-}
-if (signature && signature != ''){
-    $notification.post(`${appName} 版本号: ${rcVersion}`, `📴响应签名验证`)
-    $done({});
-}
+// if (nonce && nonce.trim() !== '') {
+    // console.log(`x-nonce detected: ${nonce}`);
+    // $notification.post(`${appName} 版本号: ${rcVersion}`, `📴`)
+    // $done({});
+// }
+// if (signature && signature != ''){
+    // $notification.post(`${appName} 版本号: ${rcVersion}`, `📴响应签名验证`)
+    // $done({});
+// }
 // 检查 Trusted Entitlements 支持和默认状态
 if (rcVersion !== 'unknown') {
     if (compareVersions(rcVersion, '4.25.0') < 0) {
@@ -73,19 +73,14 @@ if (typeof $response == "undefined") {
         'LUTCamera': {name: 'ProVersionLifeTime', id: 'com.uzero.funforcam.lifetimepurchase'},
         // 目标地图
         '%E7%9B%AE%E6%A0%87%E5%9C%B0%E5%9B%BE':{ name: 'pro', id: 'com.happydogteam.relax.lifetimePro'},
-        // 极简日记
-        'MinimalDiary':{ name: 'pro', id: 'com.mad.MinimalDiary.lifetime'},
         'Spark%20Desktop': {name: 'premium', id: 'spark_b_4199_1y_1w0'},
         'Spark': {name: 'premium', id: 'spark_b_4199_1y_1w0'},
         // PureLibro
         // 'Reader':{name:'pro', id: 'reader.lifetime.pro'},
         'Percento':{name:'premium',id:'com'},
         'AudioPlayer':{name:'Pro', id:'com'},
-        'Anybox':{name:'pro', id:'com'},
-        'MoneyThings': {name:'Premium', id:'com'},
         // 花样文字
         'UTC': {name:'Entitlement.Pro', id:'com'},
-        'Structured': {name:'pro', id:'com'},
         'VSCO': {name: 'pro', id: 'vscopro_global_5999_annual_AutoFreeTrial'},
         'GoodThing':{ name: 'pro', id: 'goodhappens_basic_forever'},
     };
