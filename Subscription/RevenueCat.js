a78cec8b5b36fbb36fb8ad181dbd74b6d689d6b4
0ee <45503268+0ee@users.noreply.github.com>
2025-07-22 15:28:19 +0800

    Update RevenueCat.js

--- Subscription/RevenueCat.js
+++ Subscription/RevenueCat.js
@@ -21,15 +21,9 @@ const UA = $request.headers['user-agent'] || $request.headers['User-Agent'] || '
 const appName = UA.split('/')[0] || 'unknown';
 const nonce = $request.headers['x-nonce'] || $request.headers['X-Nonce'];
 const signature = $request.headers['x-signature'] || $request.headers['X-Signature'];
-// if (nonce && nonce.trim() !== '') {
-    // console.log(`x-nonce detected: ${nonce}`);
-    // $notification.post(`${appName} 版本号: ${rcVersion}`, `📴`)
-    // $done({});
-// }
-// if (signature && signature != ''){
-    // $notification.post(`${appName} 版本号: ${rcVersion}`, `📴响应签名验证`)
-    // $done({});
-// }
+if ((nonce && nonce.trim() !== '') || (signature && signature !== '')) {
+    $done({});
+}
 // 检查 Trusted Entitlements 支持和默认状态
 if (rcVersion !== 'unknown') {
     if (compareVersions(rcVersion, '4.25.0') < 0) {
