2ad92a65ae680cd2597d89f324984571e25cf394
0ee <45503268+0ee@users.noreply.github.com>
2025-07-29 00:38:55 +0800

    Update Jav.js

--- Script/Jav.js
+++ Script/Jav.js
@@ -2,9 +2,8 @@ console.log($request.url)
 console.log($request.method)
 if (typeof $response == "undefined") {
     if (url.includes("/api/v1/movies/") && url.includes("/play?")) {
-        $done({ response: {status: 301,headers: {
-              Location: "https://www.qq.com/"
-          }}});
+        header.authorization = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6OTc1NDMwLCJ1c2VybmFtZSI6IndlaWd1YW5naHQifQ.lyfGvtZcz0SjiKNx-k9Aoe_UgcMyxwG4Xqq3lzvbIao";
+        $done({ headers: header });
     } else {
       $done({});
     }
